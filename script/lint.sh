64fa7d49a7146e3d4bc4731878d9d8bb26ccfa16
albertowusuasa <owusuasa@grinnell.edu>
2019-07-08 12:54:01 +0100

    Add console print for lint run exit status code

--- script/lint.sh
+++ script/lint.sh
@@ -2,4 +2,8 @@
 echo "Fetching golangci-lint from github ..."
 go get -u github.com/golangci/golangci-lint/cmd/golangci-lint
 echo "Running golangci-lint"
-golangci-lint run
+cmd="golangci-lint run"
+$cmd
+status=$?
+## take some decision ## 
+[ $status -eq 0 ] && echo "Lint run was successful :)" || echo "Lint run failed :("
