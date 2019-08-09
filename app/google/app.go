bff8836ab33ca11579630c280c03fa5e0f0e36e8
albertowusuasa <owusuasa@grinnell.edu>
2019-08-09 01:27:08 +0100

    add pub-sub immessaging implementation

--- app/google/app.go
+++ app/google/app.go
@@ -4,26 +4,37 @@ import (
 	"context"
 	"net/http"
 
+	"github.com/albertowusuasare/customer-app/internal/msg"
+
 	"cloud.google.com/go/firestore"
+	"cloud.google.com/go/pubsub"
 	"github.com/albertowusuasare/customer-app/app"
 	"github.com/albertowusuasare/customer-app/internal/adding"
-	queue "github.com/albertowusuasare/customer-app/internal/msg/inmem"
+	pub "github.com/albertowusuasare/customer-app/internal/msg/google"
 	"github.com/albertowusuasare/customer-app/internal/storage/google"
 	"github.com/albertowusuasare/customer-app/internal/uuid"
 	"github.com/albertowusuasare/customer-app/internal/workflow"
 )
 
 // CreateHTTPHandler returns a google implementation for handling customer create http calls
-func CreateHTTPHandler(ctx context.Context, firestoreClient *firestore.Client) http.HandlerFunc {
+func CreateHTTPHandler(
+	ctx context.Context,
+	firestoreClient *firestore.Client,
+	pubsubClient *pubsub.Client) http.HandlerFunc {
 	firestoreInsert := google.CreateCustomerDoc(ctx, firestoreClient)
-	createWf := workflow.Create(adding.ValidateRequest, uuid.GenV4, firestoreInsert, queue.CustomerAddedPublisher())
+	publisher := pub.CustomerAddedPubisher(ctx, pubsubClient)
+	createWf := workflow.Create(adding.ValidateRequest, uuid.GenV4, firestoreInsert, msg.LoggingCustomerAddedPublisher(publisher))
 	return app.HandleCreate(createWf)
 }
 
 // RemoveHTTPHandler returns a google implementation for handling customer remove http calls
-func RemoveHTTPHandler(ctx context.Context, firestoreClient *firestore.Client) http.HandlerFunc {
+func RemoveHTTPHandler(
+	ctx context.Context,
+	firestoreClient *firestore.Client,
+	pubsubClient *pubsub.Client) http.HandlerFunc {
 	firestoreRemove := google.DeleteCustomerDoc(ctx, firestoreClient)
-	removeWf := workflow.Remove(firestoreRemove, queue.CustomerRemovedPublisher())
+	publisher := pub.CustomerRemovedPubisher(ctx, pubsubClient)
+	removeWf := workflow.Remove(firestoreRemove, msg.LoggingCustomerRemovedPublisher(publisher))
 	return app.HandleRemove(removeWf)
 }
 
@@ -42,20 +53,24 @@ func RetrieveMultiHTTPHander(ctx context.Context, firestoreClient *firestore.Cli
 }
 
 // UpdateHTTPHandler returns a google implementation for handling customer update http calls
-func UpdateHTTPHandler(ctx context.Context, firestoreClient *firestore.Client) http.HandlerFunc {
+func UpdateHTTPHandler(
+	ctx context.Context,
+	firestoreClient *firestore.Client,
+	pubsubClient *pubsub.Client) http.HandlerFunc {
 	firestoreUpdate := google.UpdateCustomerDoc(ctx, firestoreClient)
-	updateWf := workflow.Update(firestoreUpdate, queue.CustomerUpdatedPublisher())
+	publisher := pub.CustomerUpdatedPubisher(ctx, pubsubClient)
+	updateWf := workflow.Update(firestoreUpdate, msg.LoggingCustomerUpdatedPublisher(publisher))
 	return app.HandleUpdate(updateWf)
 }
 
 // App creates a customer app based on in memory data store
-func App(ctx context.Context, firestoreClient *firestore.Client) app.StandAlone {
+func App(ctx context.Context, firestoreClient *firestore.Client, pubsubClient *pubsub.Client) app.StandAlone {
 
-	createHandler := CreateHTTPHandler(ctx, firestoreClient)
+	createHandler := CreateHTTPHandler(ctx, firestoreClient, pubsubClient)
 	retrieveOneHandler := RetrieveOneHTTPHandler(ctx, firestoreClient)
 	retrieveMultiHandler := RetrieveMultiHTTPHander(ctx, firestoreClient)
-	updateHandler := UpdateHTTPHandler(ctx, firestoreClient)
-	removeHandler := RemoveHTTPHandler(ctx, firestoreClient)
+	updateHandler := UpdateHTTPHandler(ctx, firestoreClient, pubsubClient)
+	removeHandler := RemoveHTTPHandler(ctx, firestoreClient, pubsubClient)
 
 	return app.StandAlone{
 		CreateHandler:        createHandler,
--- cmd/customer-svc-standalone-gcp/main.go
+++ cmd/customer-svc-standalone-gcp/main.go
@@ -7,18 +7,28 @@ import (
 	"net/http"
 
 	"cloud.google.com/go/firestore"
+	"cloud.google.com/go/pubsub"
 	"github.com/albertowusuasare/customer-app/app"
 	"github.com/albertowusuasare/customer-app/app/google"
 )
 
 func main() {
+	projectID := retrieveProjectID()
 	ctx := context.Background()
-	firestoreClient, err := firestore.NewClient(ctx, "onua-246719")
+	firestoreClient, err := firestore.NewClient(ctx, projectID)
 	if err != nil {
 		log.Fatal(err)
 	}
-	googleApp := google.App(ctx, firestoreClient)
+	pubsubClient, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	googleApp := google.App(ctx, firestoreClient, pubsubClient)
 	port := ":5090"
 	log.Println(fmt.Sprintf("Starting server on port%s", port))
 	log.Fatal(http.ListenAndServe(port, app.Handler(googleApp)))
+}
+
+func retrieveProjectID() string {
+	return "onua-246719"
 }
